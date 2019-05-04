ae616137363a77d1e58cc208f220c12a861e10ff
user <user@users-MacBook-Pro-4.local>
2019-05-04 15:21:42 -0500

    Un-stripe?

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,27 +1,27 @@
-$(document).ready(function() {
-  var stripe = Stripe('pk_test_m6S2QrsmCdkPcT113rYXOwAk00gen3H0dz');
-  var $checkoutButton = $("#checkout-btn");
-  var $token = $('meta[name="csrf-token"]').prop("content");
+// $(document).ready(function() {
+//   var stripe = Stripe('pk_test_m6S2QrsmCdkPcT113rYXOwAk00gen3H0dz');
+//   var $checkoutButton = $("#checkout-btn");
 
-  $checkoutButton.on("click", function() {
-    event.preventDefault();
-    event.stopPropagation();
 
-    $.ajax({
-      url: "/charges",
-      type: "post",
-      headers: {"X-CSRF-TOKEN": $token},
-      dataType: "json",
-      success: function(response) {
-        stripe.redirectToCheckout({
-          sessionId: response.charge.id
-        }).then(function (result) {
-          alert(result.error.message);
-        });
-      },
-      error: function(response) {
-        console.log(response);
-      }
-    });
-  });
-});
+//   $checkoutButton.on("click", function() {
+//     event.preventDefault();
+//     event.stopPropagation();
+
+//     $.ajax({
+//       url: "/charges",
+//       type: "post",
+//       headers: {"X-CSRF-TOKEN": $token},
+//       dataType: "json",
+//       success: function(response) {
+//         stripe.redirectToCheckout({
+//           sessionId: response.charge.id
+//         }).then(function (result) {
+//           alert(result.error.message);
+//         });
+//       },
+//       error: function(response) {
+//         console.log(response);
+//       }
+//     });
+//   });
+// });
